db752480af4defb6905568a22ebf7c4a4f7fb0a0
Squawkykaka <squawkykaka@gmail.com>
2025-04-10 15:48:56 +1200

    whoops back to 24.11

--- flake.nix
+++ flake.nix
@@ -47,15 +47,15 @@
 
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     stylix = {
-      url = "github:danth/stylix";
+      url = "github:danth/stylix/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
