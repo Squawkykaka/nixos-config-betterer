b6bf5993454c212ccd0f88da8db0d1b738fa1609
Squawkykaka <squawkykaka@gmail.com>
2025-06-10 08:33:53 +1200

    add a todo

---
+++ README.md
@@ -0,0 +1,7 @@
+## Things to Do
+
+- [ ] add ssh keys and ssh hosts to a secrets file using sops-nix
+- [ ] setup git authentication using sops-nix
+- [ ] fix somewhat laggy boot time
+- [ ] remove some caches so that download time is reduced.
+- [ ]
--- hosts/nixos/sabaton/default.nix
+++ hosts/nixos/sabaton/default.nix
@@ -18,6 +18,7 @@
     #
 
     ./hardware-configuration.nix
+    # ./wireguard.nix
     inputs.hardware.nixosModules.lenovo-thinkpad-x1-extreme-gen2
     inputs.disko.nixosModules.disko
 
@@ -114,67 +115,11 @@
     };
   };
 
-  # add gaming config.
-  specialisation = {
-    gaming-time.configuration = {
-
-      hardware.nvidia = {
-        prime.sync.enable = lib.mkForce true;
-        prime.offload = {
-          enable = lib.mkForce false;
-          enableOffloadCmd = lib.mkForce false;
-        };
-      };
-
-    };
-  };
-
   # make sure sbctl is enabled for this machine
   environment.systemPackages = [
     pkgs.sbctl
     pkgs.timeshift
   ];
-
-  networking.firewall = {
-    allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
-  };
-
-  # Enable WireGuard
-  networking.wireguard.interfaces = {
-    # "wg0" is the network interface name. You can name the interface arbitrarily.
-    wg0 = {
-      # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "192.168.2.2/32" ];
-      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
-
-      # Path to the private key file.
-      #
-      # Note: The private key can also be included inline via the privateKey option,
-      # but this makes the private key world-readable; thus, using privateKeyFile is
-      # recommended.
-      privateKeyFile = "/home/gleask/nixos/.wireguard/privkey.key";
-
-      peers = [
-        # For a client configuration, one peer entry for the server will suffice.
-
-        {
-          # Public key of the server (not a file path).
-          publicKey = "R21ssP9diNXEptvPyDOiEneZ2ZXI/c8Th7bJ9Bl1R0M=";
-
-          # Forward all the traffic via VPN.
-          allowedIPs = [ "0.0.0.0/0" ];
-          # Or forward only particular subnets
-          #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
-
-          # Set this to the server IP and port.
-          endpoint = "203.211.120.109:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
-
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
 
   # enable auto-cpu freq, disable power profiles as it interferes
   services.power-profiles-daemon.enable = false;
