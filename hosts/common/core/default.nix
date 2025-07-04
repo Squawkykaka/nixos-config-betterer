7f64ab219b26b8b9e7274b965cd0ab882eecaf51
Squawkykaka <squawkykaka@gmail.com>
2025-07-04 15:38:40 +1200

    Updated build script

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -15,14 +15,12 @@
     ])
 
     ./stylix.nix
-    ./nixos.nix
     ./sops.nix
   ];
 
   #
   # ========== Core Host Specifications ==========
   #
-
   hostSpec = {
     username = "gleask";
     handle = "squawkykaka";
@@ -40,7 +38,6 @@
   #
   # ========== Overlays ==========
   #
-
   nixpkgs = {
     # overlays = [
     #   outputs.overlays.default
@@ -96,5 +93,55 @@
         "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
       ];
     };
+  };
+
+  # Database for aiding terminal-based programs
+  environment.enableAllTerminfo = true;
+  # Enable firmware with a license allowing redistribution
+  hardware.enableRedistributableFirmware = true;
+
+  # This should be handled by config.security.pam.sshAgentAuth.enable
+  security.sudo.extraConfig = ''
+    Defaults lecture = never # rollback results in sudo lectures after each reboot, it's somewhat useless anyway
+    Defaults pwfeedback # password input feedback - makes typed password visible as asterisks
+    Defaults timestamp_timeout=120 # only ask for password every 2h
+    # Keep SSH_AUTH_SOCK so that pam_ssh_agent_auth.so can do its magic.
+    Defaults env_keep+=SSH_AUTH_SOCK
+  '';
+
+  #
+  # ========== Nix Helper ==========
+  #
+  # Provide better build output and will also handle garbage collection in place of standard nix gc (garbace collection)
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 20d --keep 5";
+    flake = "${config.hostSpec.home}/nixos";
+  };
+
+  security.polkit.enable = true;
+  security.rtkit.enable = true;
+  security.soteria.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+
+  users.mutableUsers = false;
+
+  #
+  # ========== Localization ==========
+  #
+  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
+  time.timeZone = lib.mkDefault "Pacific/Auckland";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 }
