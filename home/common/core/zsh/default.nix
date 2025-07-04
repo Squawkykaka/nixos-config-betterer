03126016b4bef256e152a84dedb35832cbeabf6e
Squawkykaka <squawkykaka@gmail.com>
2025-07-04 16:06:04 +1200

    Removed  dead code

--- home/common/core/zsh/default.nix
+++ home/common/core/zsh/default.nix
@@ -1,6 +1,5 @@
 {
   config,
-  hostSpec,
   ...
 }:
 {
@@ -24,28 +23,6 @@
 
     history.size = 10000;
     history.path = "${config.xdg.dataHome}/zsh/history";
-
-    # plugins = [
-    #   {
-    #     name = "powerlevel10k-config";
-    #     src = ./p10k;
-    #     file = "p10k.zsh.theme";
-    #   }
-    #   {
-    #     name = "zsh-powerlevel10k";
-    #     src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
-    #     file = "powerlevel10k.zsh-theme";
-    #   }
-    # ];
-
-    # initExtraFirst = ''
-    #   # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-    #   # Initialization code that may require console input (password prompts, [y/n]
-    #   # confirmations, etc.) must go above this block; everything else may go below.
-    #   if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-    #     source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-    #   fi
-    # '';
 
     initContent = ''
       # autoSuggestions config
