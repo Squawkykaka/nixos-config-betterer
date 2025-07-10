123d450aa3bc3b022f42f0f1887b30b9f5d8762e
Squawkykaka <squawkykaka@gmail.com>
2025-07-10 19:32:31 +1200

    Add some dependancies for my neovim

--- home/common/core/neovim.nix
+++ home/common/core/neovim.nix
@@ -8,7 +8,9 @@
       lua-language-server
       python311Packages.python-lsp-server
       nixd
+      lua-language-server
       gcc
+      nodejs_24
       vimPlugins.nvim-treesitter-parsers.hyprlang
     ];
   };
