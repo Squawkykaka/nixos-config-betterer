8dee26e03896ba37973a50901e08fb627db2c1d8
Squawkykaka <squawkykaka@gmail.com>
2025-11-15 12:49:14 +1300

    tinymist updated in nixpkgs, remove overide

--- home/common/core/neovim.nix
+++ home/common/core/neovim.nix
@@ -14,27 +14,11 @@
       cargo
       fd
       stylua
-      typstyle
       tree-sitter
       go
       python3
       gcc
-      (tinymist.overrideAttrs (prev: rec {
-        version = "0.14.0";
-        src = fetchFromGitHub {
-          owner = "Myriad-Dreamin";
-          repo = "tinymist";
-          tag = "v${version}";
-
-          hash = "sha256-0b9gB7vHvw1wjoTxWcQOcshpuIKfcaQJeI8GCo+rvF4=";
-        };
-
-        cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-          inherit src;
-          hash = "sha256-ctyb/llOYBiyBwKylacpXrEp3hXix64zwHxv/E54rrM=";
-        };
-      }))
-
+      tinymist
       nodejs_24
       typst
       vimPlugins.nvim-treesitter-parsers.hyprlang
