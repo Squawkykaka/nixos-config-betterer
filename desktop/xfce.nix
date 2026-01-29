1903491a7cad8c1ab6de20d578ca80208ec6a160
Squawkykaka <squawkykaka@gmail.com>
2026-01-30 11:05:23 +1300

    add xfce

---
+++ desktop/xfce.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = [
+    pkgs.nitrogen
+  ];
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce = {
+        enable = true;
+        noDesktop = true;
+        enableXfwm = false;
+
+      };
+    };
+    windowManager.bspwm = {
+      enable = true;
+    };
+  };
+  # services.displayManager.defaultSession = "xfce";
+}
