ce2edbc244fc955d8e40c489410e6c867e6a7224
Squawkykaka <squawkykaka@gmail.com>
2026-07-05 14:28:55 +1200

    update

--- wrappers/mangowc/default.nix
+++ wrappers/mangowc/default.nix
@@ -1,5 +1,6 @@
 _: {
   options = {
+    package.defaultFunc = { inputs }: inputs.nixpkgs.pkgs.mango;
     configFile.default = toString ./config.conf;
     autostartFile.default = toString ./autostart.sh;
   };
