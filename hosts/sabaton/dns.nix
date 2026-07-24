d9bb1be2873e44ca5fe907d64f0264191e1bae3e
Squawkykaka <contact@squawkykaka.com>
2026-07-25 08:04:50 +1200

    use adguard servers not on vpn

--- hosts/sabaton/dns.nix
+++ hosts/sabaton/dns.nix
@@ -1,38 +1,22 @@
-{ config, pkgs, ... }: {
-  services.dnscrypt-proxy = {
+{
+  services.dnsproxy = {
     enable = true;
     settings = {
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
-        ];
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3"; # See https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
-        cache_file = "/var/lib/${config.systemd.services.dnscrypt-proxy.serviceConfig.StateDirectory}/public-resolvers.md";
-      };
-
-      # Use servers reachable over IPv6 -- Do not enable if you don't have IPv6 connectivity
-      ipv6_servers = true;
-      block_ipv6 = false;
-
-      require_dnssec = true;
-      require_nolog = false;
-      require_nofilter = true;
-
-      # If you want, choose a specific set of servers that come from your sources.
-      # Here it's from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
-      # If you don't specify any, dnscrypt-proxy will automatically rank servers
-      # that match your criteria and choose the best one.
-      # server_names = [ ... ];
-      forwarding_rules = pkgs.writeText "forwarding_rules.txt" ''
-        boom.boats 192.168.1.48
-      '';
+      # Plain DNS upstream
+      upstream = [
+        "tls://dns.adguard.com"
+        "https://dns.adguard.com/dns-query"
+      ];
+      listen-addrs = [ "127.0.0.1" ];
+      # Plain DNS server
+      listen-ports = [ 53 ];
     };
+    # Additional launch flags
+    flags = [ "--verbose" ];
   };
   networking = {
     nameservers = [
-      "1.1.1.1"
-      "1.0.0.1"
+      "127.0.0.1"
     ];
     networkmanager = {
       enable = true;
